6f9283f7f362f58490ccbdafdff91bb73f4f87c1
Arshan Ali <you@example.comaliarshan1934@gmail.com>
2022-03-18 11:50:33 +0530

    bst insert and search functionality

---
+++ BST.js
@@ -0,0 +1,128 @@
+/* Binary Search Tree */
+
+class Node {
+    constructor(data, left = null, right = null) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+  
+  class BST {
+    constructor() {
+      this.root = null;
+    }
+    insert(data) {
+      const node = this.root;
+      if (node === null) {
+        this.root = new Node(data);
+        return;
+      } else {
+        const searchTree = function(node) {
+          if (data < node.data) {
+            if (node.left === null) {
+              node.left = new Node(data);
+              return;
+            } else if (node.left !== null) {
+              return searchTree(node.left);
+            }
+          } else if (data > node.data) {
+            if (node.right === null) {
+              node.right = new Node(data);
+              return;
+            } else if (node.right !== null) {
+              return searchTree(node.right);
+            }
+          } else {
+            return null;
+          }
+        };
+        return searchTree(node);
+      }
+    }
+    search(data) {
+      let current = this.root;
+      while (current.data !== data) {
+        if (data < current.data) {
+          current = current.left;
+        } else {
+          current = current.right;
+        }
+        if (current === null) {
+          return null;
+        }
+      }
+      return current;
+    }
+    isPresent(data) {
+      let current = this.root;
+      while (current) {
+        if (data === current.data) {
+          return true;
+        }
+        if (data < current.data) {
+          current = current.left;
+        } else {
+          current = current.right;
+        }
+      }
+      return false;
+    }
+    remove(data) {
+      const removeNode = function(node, data) {
+        if (node == null) {
+          return null;
+        }
+        if (data == node.data) {
+          // node has no children 
+          if (node.left == null && node.right == null) {
+            return null;
+          }
+          // node has no left child 
+          if (node.left == null) {
+            return node.right;
+          }
+          // node has no right child 
+          if (node.right == null) {
+            return node.left;
+          }
+          // node has two children 
+          var tempNode = node.right;
+          while (tempNode.left !== null) {
+            tempNode = tempNode.left;
+          }
+          node.data = tempNode.data;
+          node.right = removeNode(node.right, tempNode.data);
+          return node;
+        } else if (data < node.data) {
+          node.left = removeNode(node.left, data);
+          return node;
+        } else {
+          node.right = removeNode(node.right, data);
+          return node;
+        }
+      }
+      this.root = removeNode(this.root, data);
+    }
+    
+}
+  
+  
+  
+  const bst = new BST();
+  
+  bst.insert(9);
+  bst.insert(4);
+  bst.insert(17);
+  bst.insert(3);
+  bst.insert(6);
+  console.log(bst.search(17));
+  bst.insert(22);
+  bst.insert(5);
+  bst.insert(7);
+  console.log(bst.search(7));
+  bst.insert(20);
+  
+  bst.remove(20);
+  console.log(bst.isPresent(20));
+  
